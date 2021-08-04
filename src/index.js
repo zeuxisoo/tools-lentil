1dbebe3316e5d32e33ba5134c2edf69175ddcc90
Zeuxis Lo <seekstudio@gmail.com>
2021-08-04 14:09:25 +0800

    add base code for read the test file from tests directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('index.js');
+import fs from 'fs';
+
+(() => {
+
+    const options = process.argv.slice(2);
+
+    if (options.length !== 1) {
+        console.log("Please enter file path");
+        return;
+    }
+
+    const filePath = options[0];
+
+    if (fs.statSync(filePath).isFile() === false) {
+        console.log("The file path is not file");
+        return;
+    }
+
+    const fileContent = fs.readFileSync(filePath);
+
+    console.log(fileContent.toString());
+})();
