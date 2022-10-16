4e5917b6d00aa594d3158099c121b3e7cf9b8fd8
Zeuxis Lo <seekstudio@gmail.com>
2022-10-16 20:59:01 +0800

    add parse -f arguments to pass file path for parsing and add default dummy syntax

--- cmd/parse.v
+++ cmd/parse.v
@@ -1,15 +1,32 @@
 module cmd
 
 import cli
+import os
 
 pub fn parse() cli.Command {
 	return cli.Command{
 		name: 'parse'
 		description: 'Parse the syntax'
 		execute: parse_action
+		flags: [
+			cli.Flag{
+				flag: .string
+				name: 'file'
+				abbrev: 'f'
+				description: 'which file should be parse'
+				required: true
+			},
+		]
 	}
 }
 
 fn parse_action(cmd cli.Command) ? {
-	println('parsing') // TODO
+	file := cmd.flags.get_string('file') or { panic(err) }
+
+	if !os.exists(file) {
+		eprintln('file not exists: $file')
+		return
+	}
+
+	println(file)
 }
