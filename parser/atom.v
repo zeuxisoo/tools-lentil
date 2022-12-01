7f234d45cf3590f4598e6a6bd0dbb34a7dc33064
Zeuxis Lo <seekstudio@gmail.com>
2022-12-01 16:32:45 +0800

    cleanup the code move the read_token into if block in atom expression parser

--- parser/atom.v
+++ parser/atom.v
@@ -7,11 +7,13 @@ fn parse_atom_expression(mut parser Parser) !Expression {
 	current_token := parser.current_token
 
 	value := if current_token.kind == .atom {
-		parser.read_token()
-
 		current_token.value
 	} else {
 		''
+	}
+
+	if value != '' {
+		parser.read_token()
 	}
 
 	expression := AtomExpression{
