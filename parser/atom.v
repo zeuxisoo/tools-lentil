d9fdfb4cae8b7b137aeaf0650f82ea09bf89bcee
Zeuxis Lo <seekstudio@gmail.com>
2022-12-01 16:25:49 +0800

    fix the title expression parser should be call read_token() in token kind is .atom not all kind

--- parser/atom.v
+++ parser/atom.v
@@ -7,11 +7,12 @@ fn parse_atom_expression(mut parser Parser) !Expression {
 	current_token := parser.current_token
 
 	value := if current_token.kind == .atom {
+		parser.read_token()
+
 		current_token.value
 	} else {
 		''
 	}
-	parser.read_token()
 
 	expression := AtomExpression{
 		value: value
