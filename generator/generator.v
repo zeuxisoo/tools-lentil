e01f60d9eb1decd614fd56c5a57158495a262abe
Zeuxis Lo <seekstudio@gmail.com>
2023-01-15 21:15:22 +0800

    change the return type from ProduceType to !ProduceType in the generator produce method

--- generator/generator.v
+++ generator/generator.v
@@ -32,23 +32,27 @@ pub fn new_generator(mut parser parser.Parser) &Generator {
 }
 
 pub fn (mut g Generator) generate() !string {
-	ast_file := g.parser.parse()!
+	ast_file := g.parser.parse() or {
+		return err
+	}
 
 	mut environment := Environment{}
 	environment.add_program('root', ast_file.root)
 
-	result := g.produce(ast_file.ast, mut environment) as []string
+	result := g.produce(ast_file.ast, mut environment) or {
+		return err
+	} as []string
 
 	return result.join('\n')
 }
 
-fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType {
+fn (mut g Generator) produce(node Node, mut environment Environment) !ProduceType {
 	return match node {
 		Program {
 			mut codes := []string{}
 
 			for statement in node.statements {
-				code := g.produce(statement as Node, mut environment)
+				code := g.produce(statement as Node, mut environment)!
 
 				if code is string {
 					if code.len > 0 {
@@ -63,7 +67,7 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			node.value
 		}
 		IncludeStatement {
-			file_path := g.produce(node.path as Node, mut environment)
+			file_path := g.produce(node.path as Node, mut environment)!
 			full_path := os.join_path_single(environment.programs['root'], file_path as string)
 
 			mut lexer := lexer.new_lexer(full_path) or { panic(err) }
@@ -71,10 +75,10 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 
 			ast_file := parser.parse() or { panic(err) }
 
-			g.produce(ast_file.ast, mut environment)
+			g.produce(ast_file.ast, mut environment)!
 		}
 		ConfigStatement {
-			g.produce(node.block as Node, mut environment)
+			g.produce(node.block as Node, mut environment)!
 		}
 		ConfigBlockStatement {
 			for value in node.values {
@@ -82,8 +86,8 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 					expression := value.expression
 
 					if expression is AssignExpression {
-						name := g.produce(expression.left as Node, mut environment) as string
-						data := g.produce(expression.right as Node, mut environment)
+						name := g.produce(expression.left as Node, mut environment)! as string
+						data := g.produce(expression.right as Node, mut environment)!
 
 						if data is []string {
 							environment.add_config(name, EnvironmentConfigType(data))
@@ -96,7 +100,7 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 		}
 		DateStatement {
 			date := node.value
-			rows := g.produce(node.block as Node, mut environment) as []map[string]ProduceMapValue
+			rows := g.produce(node.block as Node, mut environment)! as []map[string]ProduceMapValue
 
 			escape_quote := fn (value string) string {
 				return value.replace('"', '\\"')
@@ -116,13 +120,13 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			contents.join('\n')
 		}
 		DateBlockStatement {
-			g.produce(node.value as Node, mut environment)
+			g.produce(node.value as Node, mut environment)!
 		}
 		DateRecordsExpression {
 			mut records := []map[string]ProduceMapValue{}
 
 			for value in node.values {
-				record := g.produce(value as Node, mut environment)
+				record := g.produce(value as Node, mut environment)!
 
 				records << record as map[string]ProduceMapValue
 			}
@@ -133,13 +137,13 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			mut receipts := []map[string]ProduceMapValue{}
 
 			for value in node.values {
-				receipt := g.produce(value as Node, mut environment)
+				receipt := g.produce(value as Node, mut environment)!
 
 				receipts << receipt as map[string]ProduceMapValue
 			}
 
-			title := g.produce(node.title as Node, mut environment) as string
-			description := g.produce(node.description as Node, mut environment) as string
+			title := g.produce(node.title as Node, mut environment)! as string
+			description := g.produce(node.description as Node, mut environment)! as string
 
 			mut records := map[string]ProduceMapValue{}
 			records['title'] = title
@@ -149,19 +153,26 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			records
 		}
 		DateRecordReceiptExpression {
-			account_name := g.produce(node.account as Node, mut environment) as string
+			account_name := g.produce(node.account as Node, mut environment)! as string
 			account_value := environment.variables[account_name] or {
-				panic('generator: account name not found, got `${account_name}`')
+				return error('generator: account name not found, got `${account_name}`')
 			} as string
 
 			// find longest account to calculate whitespace between account and currency
-			longest_account := arrays.reduce(environment.variables.values(), fn (a EnvironmentVariableType, b EnvironmentVariableType) EnvironmentVariableType {
-				a_value := a as string
-				b_value := b as string
+			longest_account := arrays.reduce(
+				environment.variables.values(),
+				fn (a EnvironmentVariableType, b EnvironmentVariableType) EnvironmentVariableType {
+					a_value := a as string
+					b_value := b as string
 
-				return if a_value.len > b_value.len { a } else { b }
-			}) or {
-				panic('generator: cannot found max length account in environment variable table')
+					if a_value.len > b_value.len {
+						return a
+					} else {
+						return b
+					}
+				}
+			) or {
+				return error('generator: cannot found max length account in environment variable table')
 			}
 
 			whitespace_length := (longest_account as string).len - account_value.len + 4
@@ -169,15 +180,15 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			// find manually input amount
 			amounts := node.amounts
 
-			amount := if amounts is AmountsExpression {
+			mut amount := []map[string]ProduceMapValue{}
+
+			if amounts is AmountsExpression {
 				if amounts.values.len > 0 {
-					g.produce(amounts, mut environment)
-				} else {
-					[]map[string]ProduceMapValue{}
+					amount = g.produce(amounts, mut environment)! as []map[string]ProduceMapValue
 				}
-			} else {
-				panic('generator: expected amount expression, but got `${amounts}`')
-			} as []map[string]ProduceMapValue
+			}else{
+				return error('generator: expected amount expression, but got `${amounts}`')
+			}
 
 			// return ProduceType
 			mut receipts := map[string]ProduceMapValue{}
@@ -189,20 +200,20 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			receipts
 		}
 		ExpressionStatement {
-			g.produce(node.expression as Node, mut environment)
+			g.produce(node.expression as Node, mut environment)!
 		}
 		ArrayExpression {
 			mut items := []string{}
 
 			for value in node.values {
-				items << g.produce(value as Node, mut environment) as string
+				items << g.produce(value as Node, mut environment)! as string
 			}
 
 			items
 		}
 		AssignExpression {
-			name := g.produce(node.left as Node, mut environment) as string
-			data := g.produce(node.right as Node, mut environment)
+			name := g.produce(node.left as Node, mut environment)! as string
+			data := g.produce(node.right as Node, mut environment)!
 
 			if data is string {
 				environment.add_variable(name, EnvironmentVariableType(data))
@@ -214,14 +225,14 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			mut amounts := []map[string]ProduceMapValue{}
 
 			for amount in node.values {
-				amounts << g.produce(amount as Node, mut environment) as map[string]ProduceMapValue
+				amounts << g.produce(amount as Node, mut environment)! as map[string]ProduceMapValue
 			}
 
 			amounts
 		}
 		AmountExpression {
-			price := g.produce(node.value as Node, mut environment) as string
-			currency := g.produce(node.currency as Node, mut environment) as string
+			price := g.produce(node.value as Node, mut environment)! as string
+			currency := g.produce(node.currency as Node, mut environment)! as string
 
 			mut amount := map[string]ProduceMapValue{}
 			amount['price'] = price
@@ -230,13 +241,13 @@ fn (mut g Generator) produce(node Node, mut environment Environment) ProduceType
 			amount
 		}
 		NumberExpression {
-			kind := g.produce(node.kind as Node, mut environment) as string
+			kind := g.produce(node.kind as Node, mut environment)! as string
 			value := node.value
 
 			'${kind}${value}'
 		}
 		else {
-			panic('generator: unknown node: ${node}')
+			return error('generator: unknown node: ${node}')
 		}
 	}
 }
